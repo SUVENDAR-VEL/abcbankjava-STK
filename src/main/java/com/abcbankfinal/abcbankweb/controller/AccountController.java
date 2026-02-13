6e673c965b91e436721e998389e6a9f646f98f7f
Suvendar T <suvendar.t@primesoftinc.com>
2026-02-13 12:32:20 +0530

    crros

--- src/main/java/com/abcbankfinal/abcbankweb/controller/AccountController.java
+++ src/main/java/com/abcbankfinal/abcbankweb/controller/AccountController.java
@@ -6,16 +6,14 @@ import com.abcbankfinal.abcbankweb.service.TransactionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/account")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class AccountController {
 
     @Autowired
