c6d66ccd89c8ce6536992b1c3a03ad233357e334
Suvendar T <suvendar.t@primesoftinc.com>
2026-02-17 14:27:58 +0530

    user_22

--- src/main/java/com/abcbankfinal/abcbankweb/repository/AccountRepository.java
+++ src/main/java/com/abcbankfinal/abcbankweb/repository/AccountRepository.java
@@ -17,45 +17,90 @@ import java.util.List;
 public interface AccountRepository extends JpaRepository<Account, Long> {
     List<Account> findByCustomerUserId(Long userId);
 
+//    @Query(value = """
+//    SELECT
+//        u.user_id AS userId,
+//        u.first_name AS firstName,
+//        u.last_name AS lastName,
+//        u.email AS email,
+//        u.mobile_number AS mobileNumber,
+//        u.alternative_number AS alternativeNumber,
+//        u.address AS address,
+//        u.city AS city,
+//        u.state AS state,
+//        u.country AS country,
+//        u.pincode AS pincode,
+//        u.pancard AS pancard,
+//        u.aadhar AS aadhar,
+//        u.date_of_birth AS dateOfBirth,
+//        u.created_date AS createdDate,
+//
+//        r.role_id AS roleId,
+//        r.role_name AS roleName,
+//
+//        a.account_number AS accountNumber,
+//        a.balance AS balance,
+//        a.status AS accountStatus,
+//        a.branch_name AS branchName,
+//        a.branch_code AS branchCode,
+//        a.opened_date AS openedDate,
+//
+//        at.account_type_id AS accountTypeId,
+//        at.account_type_name AS accountTypeName
+//
+//    FROM account a
+//    JOIN user u ON a.customer_id = u.user_id
+//    JOIN role r ON u.role_id = r.role_id
+//    JOIN account_type at ON a.account_type_id = at.account_type_id
+//    WHERE a.account_number = :accountNumber
+//    """, nativeQuery = true)
+//    AccountFullDetailsProjection findAccountFullDetails(
+//            @Param("accountNumber") Long accountNumber);
+
+
+
+
+
     @Query(value = """
-    SELECT 
-        u.user_id AS userId,
-        u.first_name AS firstName,
-        u.last_name AS lastName,
-        u.email AS email,
-        u.mobile_number AS mobileNumber,
-        u.alternative_number AS alternativeNumber,
-        u.address AS address,
-        u.city AS city,
-        u.state AS state,
-        u.country AS country,
-        u.pincode AS pincode,
-        u.pancard AS pancard,
-        u.aadhar AS aadhar,
-        u.date_of_birth AS dateOfBirth,
-        u.created_date AS createdDate,
+SELECT 
+    u.user_id AS userId,
+    u.first_name AS firstName,
+    u.last_name AS lastName,
+    u.email AS email,
+    u.mobile_number AS mobileNumber,
+    u.alternative_number AS alternativeNumber,
+    u.address AS address,
+    u.city AS city,
+    u.state AS state,
+    u.country AS country,
+    u.pincode AS pincode,
+    u.pancard AS pancard,
+    u.aadhar AS aadhar,
+    u.date_of_birth AS dateOfBirth,
+    u.created_date AS createdDate,
 
-        r.role_id AS roleId,
-        r.role_name AS roleName,
+    r.role_id AS roleId,
+    r.role_name AS roleName,
 
-        a.account_number AS accountNumber,
-        a.balance AS balance,
-        a.status AS accountStatus,
-        a.branch_name AS branchName,
-        a.branch_code AS branchCode,
-        a.opened_date AS openedDate,
+    a.account_number AS accountNumber,
+    a.balance AS balance,
+    a.status AS accountStatus,
+    a.branch_name AS branchName,
+    a.branch_code AS branchCode,
+    a.opened_date AS openedDate,
 
-        at.account_type_id AS accountTypeId,
-        at.account_type_name AS accountTypeName
+    at.account_type_id AS accountTypeId,
+    at.account_type_name AS accountTypeName
 
-    FROM account a
-    JOIN user u ON a.customer_id = u.user_id
-    JOIN role r ON u.role_id = r.role_id
-    JOIN account_type at ON a.account_type_id = at.account_type_id
-    WHERE a.account_number = :accountNumber
-    """, nativeQuery = true)
+FROM account a
+JOIN user u ON a.customer_id = u.user_id
+JOIN role r ON u.role_id = r.role_id
+LEFT JOIN account_type at ON a.account_type_id = at.account_type_id
+WHERE a.account_number = :accountNumber
+""", nativeQuery = true)
     AccountFullDetailsProjection findAccountFullDetails(
             @Param("accountNumber") Long accountNumber);
+
 
 
 
